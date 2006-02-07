fbc0a82feba85139fb6a7e9133e94cbd3fcf835c
makoto kuroeda <makotan@gmail.com>
2006-02-07 05:59:41 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@13 1c266114-290a-0410-b1ce-a63319e86887

--- core/src/org/seasar/buri/common/util/ExcelIOUtil.java
+++ core/src/org/seasar/buri/common/util/ExcelIOUtil.java
@@ -4,28 +4,49 @@
  */
 package org.seasar.buri.common.util;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
+import org.seasar.framework.exception.IORuntimeException;
 import org.seasar.framework.log.Logger;
+import org.seasar.framework.util.FileInputStreamUtil;
 import org.seasar.framework.util.ResourceUtil;
 
 public class ExcelIOUtil {
     private static Logger logger = Logger.getLogger(ExcelIOUtil.class);
 
+    public static Workbook getWorkbookNoResource(String fileName) {
+        File file = new File(fileName);
+        InputStream is = FileInputStreamUtil.create(file);
+        Workbook workbook = getWorkbook(is);
+        return workbook;
+    }
+
     public static Workbook getWorkbook(String fileName) {
         URL resource = ResourceUtil.getResource(fileName);
         Workbook workbook = null;
         try {
-            workbook = Workbook.getWorkbook(resource.openStream());
+            workbook = getWorkbook(resource.openStream());
+        } catch (IOException e) {
+            throw new IORuntimeException(e);
+        }
+        return workbook;
+    }
+
+    private static Workbook getWorkbook(InputStream is) {
+        Workbook workbook = null;
+        try {
+            workbook = Workbook.getWorkbook(is);
         } catch (BiffException e) {
             logger.error(e);
         } catch (IOException e) {
-            logger.error(e);
+            throw new IORuntimeException(e);
         }
         return workbook;
     }
