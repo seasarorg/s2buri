72d29c0af0a37c676c31b880679e6d66ba2b26dd
Yasuharu Nakano <nobeans@gmail.com>
2007-03-29 14:31:26 +0000

    derby用のSQLファイルからDROP文を分離した。 pom.xmlにSQL登録用のplugin設定を追加した(コメントアウト状態で)。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@187 1c266114-290a-0410-b1ce-a63319e86887

---
+++ Buri2.3/db/dropdb_derby.sql
@@ -0,0 +1,21 @@
+DROP VIEW BuriPathDataUser;
+
+DROP VIEW BuriPathData;
+
+DROP TABLE BuriStateUser;
+
+DROP TABLE BuriState;
+
+DROP TABLE BuriStateUndoLog;
+
+DROP TABLE BuriTransaction;
+
+DROP TABLE BuriBranch;
+
+DROP TABLE BuriDataPathHistory;
+
+DROP TABLE BuriData;
+
+DROP TABLE BuriPath;
+
+DROP TABLE BuriUser;
