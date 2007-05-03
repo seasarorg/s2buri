e8cefc2a92f29435c679047ccaa2f58bc77af8df
a-conv <a-conv@1c266114-290a-0410-b1ce-a63319e86887>
2007-05-03 16:38:12 +0000

    buriのバージョン番号更新
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@223 1c266114-290a-0410-b1ce-a63319e86887

---
+++ teeda-buri-app/study-buri/src/main/java/example/org/seasar/buri/web/IndexPage.java
@@ -0,0 +1,17 @@
+package example.org.seasar.buri.web;
+
+public class IndexPage {
+
+	public String initialize() {
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+	public String getLayout() {
+		return null;
+	}
+	
+}
