7330e8eb43f5315a7f2aa87689295087ecb60066
a-conv <a-conv@1c266114-290a-0410-b1ce-a63319e86887>
2007-06-06 03:39:33 +0000

    archetype:create ぶり用 動作確認 DBの作成部分はテスト中
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@239 1c266114-290a-0410-b1ce-a63319e86887

---
+++ maven-archetype-buri/src/main/resources/archetype-resources/db/sql/h2/00_dropdb_h2.sql
@@ -0,0 +1,45 @@
+drop view BuriPathDataUser;
+
+drop view BuriPathData;
+
+drop view BuriPathHistoryDataUser;
+
+drop view BuriPathHistoryData;
+
+
+DROP TABLE BuriStateUser;
+
+
+DROP SEQUENCE BuriStateUserID;
+
+DROP TABLE BuriState;
+
+DROP SEQUENCE BuriStateID;
+
+DROP TABLE BuriStateUndoLog;
+
+DROP SEQUENCE BuriStateUndoLogID;
+
+DROP TABLE BuriTransaction;
+
+DROP SEQUENCE BuriTransactionID;
+
+DROP TABLE BuriBranch;
+
+DROP SEQUENCE BuriBranchID;
+
+DROP TABLE BuriDataPathHistory;
+
+DROP SEQUENCE BuriDataPathHistoryID;
+
+DROP TABLE BuriData;
+
+DROP SEQUENCE BuriDataID;
+
+DROP TABLE BuriPath;
+
+DROP SEQUENCE BuriPathID;
+
+DROP TABLE BuriUser;
+
+DROP SEQUENCE BuriUserID;
