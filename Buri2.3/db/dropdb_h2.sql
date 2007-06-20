92fb3f4772e89ade6bb849d7dd04c3fece5fc483
a-conv <a-conv@1c266114-290a-0410-b1ce-a63319e86887>
2007-06-20 04:11:36 +0000

    H2用SQL定義追加(テストOK確認済)、H2最新版をローカルリポジトリに追加（暫定）
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@245 1c266114-290a-0410-b1ce-a63319e86887

---
+++ Buri2.3/db/dropdb_h2.sql
@@ -0,0 +1,30 @@
+DROP VIEW BuriPathDataUser;
+
+DROP VIEW BuriPathData;
+
+DROP SEQUENCE BuriStateUserID;
+DROP TABLE BuriStateUser;
+
+DROP SEQUENCE BuriStateID;
+DROP TABLE BuriState;
+
+DROP SEQUENCE BuriStateUndoLogID;
+DROP TABLE BuriStateUndoLog;
+
+DROP SEQUENCE BuriTransactionID;
+DROP TABLE BuriTransaction;
+
+DROP SEQUENCE BuriBranchID;
+DROP TABLE BuriBranch;
+
+DROP SEQUENCE BuriDataPathHistoryID;
+DROP TABLE BuriDataPathHistory;
+
+DROP SEQUENCE BuriDataID;
+DROP TABLE BuriData;
+
+DROP SEQUENCE BuriPathID;
+DROP TABLE BuriPath;
+
+DROP SEQUENCE BuriUserID;
+DROP TABLE BuriUser;
