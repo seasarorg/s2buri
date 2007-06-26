ae05bb231dd6d144250c12876e849ef6697f0f17
a-conv <a-conv@1c266114-290a-0410-b1ce-a63319e86887>
2007-06-26 10:22:27 +0000

    BuriSignal作成、テストケースは後でコミットします m(_ _)m
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@251 1c266114-290a-0410-b1ce-a63319e86887

---
+++ Buri2.3/src/main/java/org/escafe/buri/engine/processor/util/BuriSignal.java
@@ -0,0 +1,12 @@
+package org.escafe.buri.engine.processor.util;
+
+/**
+ * 
+ * @author a-conv
+ * 
+ */
+public interface BuriSignal {
+
+	public void signal(String callPath, Object data);
+
+}
---
+++ Buri2.3/src/main/java/org/escafe/buri/engine/processor/util/impl/BuriSignalImpl.java
@@ -0,0 +1,188 @@
+package org.escafe.buri.engine.processor.util.impl;
+
+import java.util.List;
+
+import org.escafe.buri.dao.BuriDataDao;
+import org.escafe.buri.dao.BuriPathDao;
+import org.escafe.buri.dao.BuriPathDataDao;
+import org.escafe.buri.dao.BuriPathDataUserDao;
+import org.escafe.buri.dao.BuriUserDao;
+import org.escafe.buri.dao.util.BuriDataUtil;
+import org.escafe.buri.dao.util.BuriUserUtil;
+import org.escafe.buri.dto.BuriPathDataEntityDto;
+import org.escafe.buri.dto.BuriPathDataUserEntityDto;
+import org.escafe.buri.dto.BuriPathEntityDto;
+import org.escafe.buri.engine.IdentityInfo;
+import org.escafe.buri.engine.processor.BuriAutoSelectProcessor;
+import org.escafe.buri.engine.processor.BuriProcessorInfo;
+import org.escafe.buri.engine.processor.util.BuriSignal;
+import org.seasar.coffee.dataaccess.DataAccessFactory;
+import org.seasar.coffee.dataaccess.DataAccessUtil;
+import org.seasar.coffee.dataaccess.DataAccessUtilLongKey;
+import org.seasar.coffee.dataaccess.DataAccessUtilManyKey;
+
+/**
+ * 
+ * @author a-conv
+ * 
+ */
+public class BuriSignalImpl implements BuriSignal {
+
+	private BuriAutoSelectProcessor processor;
+
+	private BuriPathDao pathDao;
+
+	private BuriPathDataDao pathDataDao;
+
+	private BuriDataUtil dataUtil;
+
+	private BuriDataDao dataDao;
+
+	private BuriUserUtil userUtil;
+
+	private BuriUserDao userDao;
+
+	private BuriPathDataUserDao pathDataUserDao;
+
+	private Long getLongKey(Object data, DataAccessFactory accessFactory) {
+		Long longKey = null;
+		DataAccessUtil accessUtil = accessFactory.getDataAccessUtil(data.getClass());
+		if (accessUtil instanceof DataAccessUtilLongKey) {
+			DataAccessUtilLongKey longKeyUtil = (DataAccessUtilLongKey) accessUtil;
+			longKey = longKeyUtil.getKey(data);
+		}
+		return longKey;
+	}
+
+	private String getManyKey(Object data, DataAccessFactory accessFactory) {
+		String manyKey = null;
+		DataAccessUtil accessUtil = accessFactory.getDataAccessUtil(data.getClass());
+		if (accessUtil instanceof DataAccessUtilManyKey) {
+			DataAccessUtilManyKey manyKeyUtil = (DataAccessUtilManyKey) accessUtil;
+			manyKey = manyKeyUtil.getKey(data);
+		}
+		return manyKey;
+	}
+
+	private BuriPathDataEntityDto getBuriPathData(String callPath, Object data, DataAccessFactory accessFactory) {
+		BuriPathEntityDto pathEntityDto = getBuriPath(callPath);
+		String className = data.getClass().getName();
+		String pathName = pathEntityDto.getPathName();
+		Long pathType = pathEntityDto.getPathType();
+		Long longKey = getLongKey(data, accessFactory);
+		String manyKey = getManyKey(data, accessFactory);
+		return pathDataDao.getDtoByPathKey(className, longKey, manyKey, pathName, pathType);
+	}
+
+	private BuriPathEntityDto getBuriPath(String callPath) {
+		List<BuriPathEntityDto> paths = pathDao.getAllBuriPath();
+		for (BuriPathEntityDto path : paths) {
+			if (path.getPathName().equals(callPath)) {
+				return path;
+			}
+		}
+		return null;
+	}
+
+	private Object getTargetData(String callPath, Object data, DataAccessFactory accessFactory, Long dataID) {
+		return dataUtil.getBuriData(dataID, accessFactory);
+	}
+
+	private Object getUserData(BuriPathDataEntityDto pathDataDto, DataAccessFactory accessFactory) {
+		BuriPathDataUserEntityDto pathDataUserDto = pathDataUserDao.getDto(pathDataDto.getStateID());
+		IdentityInfo appUserId = new IdentityInfo(pathDataUserDto.getUserIDNum(), pathDataUserDto.getUserIDVal());
+		return userUtil.getUserData(accessFactory, pathDataUserDto.getBuriUserID(), appUserId);
+	}
+
+	private void simpleCall(String callPath, Object data, DataAccessFactory accessFactory, BuriProcessorInfo info) {
+		BuriPathDataEntityDto pathDataDto = getBuriPathData(callPath, data, accessFactory);
+		Long dataID = pathDataDto.getDataID();
+		Object targetDto = getTargetData(callPath, data, accessFactory, dataID);
+		processor.toNextStatus(callPath, targetDto, null, info);
+	}
+
+	private void standardCall(String callPath, Object data, DataAccessFactory accessFactory, BuriProcessorInfo info) {
+		BuriPathDataEntityDto pathDataDto = getBuriPathData(callPath, data, accessFactory);
+		Long dataID = pathDataDto.getDataID();
+		Object targetDto = getTargetData(callPath, data, accessFactory, dataID);
+		Object userData = getUserData(pathDataDto, accessFactory);
+		processor.toNextStatus(callPath, targetDto, userData, info);
+	}
+
+	public void signal(String callPath, Object data) {
+		DataAccessFactory accessFactory = processor.getDataAccessFactory(callPath);
+		BuriProcessorInfo info = new BuriProcessorInfo();
+		info.put("signalAction", Boolean.TRUE);
+		if (processor.isSimpleProcessor(callPath)) {
+			simpleCall(callPath, data, accessFactory, info);
+		}
+		if (processor.isStdProcessor(callPath)) {
+			standardCall(callPath, data, accessFactory, info);
+		}
+	}
+
+	public BuriAutoSelectProcessor getProcessor() {
+		return processor;
+	}
+
+	public void setProcessor(BuriAutoSelectProcessor processor) {
+		this.processor = processor;
+	}
+
+	public BuriPathDao getPathDao() {
+		return pathDao;
+	}
+
+	public void setPathDao(BuriPathDao pathDao) {
+		this.pathDao = pathDao;
+	}
+
+	public BuriPathDataDao getPathDataDao() {
+		return pathDataDao;
+	}
+
+	public void setPathDataDao(BuriPathDataDao pathDataDao) {
+		this.pathDataDao = pathDataDao;
+	}
+
+	public BuriDataUtil getDataUtil() {
+		return dataUtil;
+	}
+
+	public void setDataUtil(BuriDataUtil dataUtil) {
+		this.dataUtil = dataUtil;
+	}
+
+	public BuriDataDao getDataDao() {
+		return dataDao;
+	}
+
+	public void setDataDao(BuriDataDao dataDao) {
+		this.dataDao = dataDao;
+	}
+
+	public BuriUserDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(BuriUserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public BuriUserUtil getUserUtil() {
+		return userUtil;
+	}
+
+	public void setUserUtil(BuriUserUtil userUtil) {
+		this.userUtil = userUtil;
+	}
+
+	public BuriPathDataUserDao getPathDataUserDao() {
+		return pathDataUserDao;
+	}
+
+	public void setPathDataUserDao(BuriPathDataUserDao pathDataUserDao) {
+		this.pathDataUserDao = pathDataUserDao;
+	}
+
+}
