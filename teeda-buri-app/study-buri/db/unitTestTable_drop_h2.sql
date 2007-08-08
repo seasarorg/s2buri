2ddb9d4542c21a657481296acbae343462d0b8f2
a-conv <a-conv@1c266114-290a-0410-b1ce-a63319e86887>
2007-08-08 05:59:13 +0000

    使用ライブラリーの更新
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@297 1c266114-290a-0410-b1ce-a63319e86887

---
+++ teeda-buri-app/study-buri/db/unitTestTable_drop_h2.sql
@@ -0,0 +1,14 @@
+DROP SEQUENCE FurnitureItemID;
+DROP TABLE FurnitureItem;
+
+DROP SEQUENCE BuriTestINTID;
+DROP TABLE BuriTestINT;
+
+DROP TABLE BuriTestCHAR ;
+
+DROP TABLE BuriTestMany ;
+
+DROP SEQUENCE BuriTestUserID;
+DROP TABLE BuriTestUser;
+
+	
