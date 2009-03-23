84df1c5d43f03297de63fdecaf10eb274719a6de
Junichi Kato <j5ik2o@gmail.com>
2009-03-23 08:26:09 +0000

    リファクタリングしました。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/branches/kato@463 1c266114-290a-0410-b1ce-a63319e86887

--- buri-core-test/src/main/resources/META-INF/sql/example/org/escafe/buri/entity/Item/findAndUser.sql
+++ buri-core-test/src/main/resources/META-INF/sql/example/org/escafe/buri/entity/Item/findAndUser.sql
@@ -15,13 +15,12 @@ WHERE
 	/*IF dto.itemId_to != null*/AND ITEM_ID <= /*dto.itemId_to*/0/*END*/
 	/*IF dto.itemId_moreSmall != null*/AND ITEM_ID <= /*dto.itemId_moreSmall*/0/*END*/
 	/*IF dto.itemId_small != null*/AND ITEM_ID < /*dto.itemId_small*/0/*END*/
-	/*IF dto.itemId_isNull != null*/AND ITEM_ID is null /*END*/
-	/*IF dto.itemId_isNotNull != null*/AND ITEM_ID is not null/*END*/
+	/*IF dto.itemId_isNull != null*/AND ITEM_ID IS null /*END*/
+	/*IF dto.itemId_isNotNull != null*/AND ITEM_ID IS NOT null/*END*/
 	/*IF dto.itemId_in != null*/AND ITEM_ID in /*dto.itemId_in*/(0)/*END*/
-
-	/*IF dto.itemName_matchFull != null*/AND ITEM_NAME Like /*dto.itemName_matchFull*/'%TestData%'/*END*/
-	/*IF dto.itemName_matchFront != null*/AND ITEM_NAME Like /*dto.itemName_matchFront*/'TestData%'/*END*/
-	/*IF dto.itemName_matchBack != null*/AND ITEM_NAME Like /*dto.itemName_matchBack*/'%TestData'/*END*/
+	/*IF dto.itemName_matchFull != null*/AND ITEM_NAME LIKE /*dto.itemName_matchFull*/'%TestData%'/*END*/
+	/*IF dto.itemName_matchFront != null*/AND ITEM_NAME LIKE /*dto.itemName_matchFront*/'TestData%'/*END*/
+	/*IF dto.itemName_matchBack != null*/AND ITEM_NAME LIKE /*dto.itemName_matchBack*/'%TestData'/*END*/
 	/*IF dto.itemName != null*/AND ITEM_NAME = /*dto.itemName*/'TestData'/*END*/
 	/*IF dto.itemName_not != null*/AND ITEM_NAME != /*dto.itemName_not*/'TestData'/*END*/
 	/*IF dto.itemName_large != null*/AND  /*dto.itemName_large*/'TestData' < ITEM_NAME/*END*/
@@ -33,8 +32,6 @@ WHERE
 	/*IF dto.itemName_isNull != null*/AND ITEM_NAME is null /*END*/
 	/*IF dto.itemName_isNotNull != null*/AND ITEM_NAME is not null/*END*/
 	/*IF dto.itemName_in != null*/AND ITEM_NAME in /*dto.itemName_in*/('TestData')/*END*/
-
-	
 	/*IF dto.price != null*/AND PRICE = /*dto.price*/0/*END*/
 	/*IF dto.price_not != null*/AND PRICE != /*dto.price_not*/0/*END*/
 	/*IF dto.price_large != null*/AND  /*dto.price_large*/0 < PRICE/*END*/
@@ -46,10 +43,8 @@ WHERE
 	/*IF dto.price_isNull != null*/AND PRICE is null /*END*/
 	/*IF dto.price_isNotNull != null*/AND PRICE is not null/*END*/
 	/*IF dto.price_in != null*/AND PRICE in /*dto.price_in*/(0)/*END*/
-
 	AND ITEM.ITEM_ID = BURI_PATH_DATA_USER.PKEY_NUM
-	/*IF paths != null*/AND BuriPathDataUser.PathName in /*paths*/('buri.path.names')/*END*/
-	/*IF userIds != null*/AND BuriPathDataUser.BuriUserID in /*userIds*/(1)/*END*/
-	
+	/*IF paths != null*/AND BURI_PATH_DATA_USER.PATH_NAME IN /*paths*/('buri.path.names')/*END*/
+	/*IF userIds != null*/AND BURI_PATH_DATA_USER.BURI_USER_ID IN /*userIds*/(1)/*END*/
 /*END*/
 /*$dto.orderList*/
