47fcd7b9e79e821791d8fddd8210cc4cf6a1613f
Junichi Kato <j5ik2o@gmail.com>
2009-03-26 07:47:28 +0000

    refactor
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/branches/kato@507 1c266114-290a-0410-b1ce-a63319e86887

--- buri-core/src/main/java/org/escafe/buri/service/BuriBranchEntityService.java
+++ buri-core/src/main/java/org/escafe/buri/service/BuriBranchEntityService.java
@@ -1,23 +1,24 @@
 package org.escafe.buri.service;
 
-import static org.escafe.buri.names.BuriBranchEntityNames.parentBranchId;
-import static org.seasar.extension.jdbc.operation.Operations.eq;
-
 import java.util.List;
 
 import org.escafe.buri.entity.BuriBranchEntity;
+
+import static org.escafe.buri.names.BuriBranchEntityNames.*;
+import static org.seasar.extension.jdbc.operation.Operations.*;
 
 public class BuriBranchEntityService extends AbstractService<BuriBranchEntity> {
 	public List<BuriBranchEntity> getAllBuriBranch() {
 		return findAll();
 	}
 
-	public BuriBranchEntity select(long branchId) {
+	public BuriBranchEntity select(Long branchId) {
 		return selectById(branchId);
 	}
 
-	public List<BuriBranchEntity> getBranchByParentID(long parentBranchID) {
-		return select().where(eq(parentBranchId(), parentBranchID))
-				.getResultList();
+	public List<BuriBranchEntity> getBranchByParentId(Long parentBranchId) {
+		return select()
+		    .where(eq(parentBranchId(), parentBranchId))
+		    .getResultList();
 	}
 }
--- buri-core/src/main/java/org/escafe/buri/service/BuriDataPathHistoryEntityService.java
+++ buri-core/src/main/java/org/escafe/buri/service/BuriDataPathHistoryEntityService.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.escafe.buri.entity.BuriDataPathHistoryEntity;
 
 public class BuriDataPathHistoryEntityService extends
-		AbstractService<BuriDataPathHistoryEntity> {
-
+        AbstractService<BuriDataPathHistoryEntity> {
 	public List<BuriDataPathHistoryEntity> getAllBuriDataPathHistory() {
 		return findAll();
 	}
 
-	public BuriDataPathHistoryEntity getBuriDataPathHistory(long historyId) {
+	public BuriDataPathHistoryEntity getBuriDataPathHistory(Long historyId) {
 		return selectById(historyId);
 	}
-
 }
