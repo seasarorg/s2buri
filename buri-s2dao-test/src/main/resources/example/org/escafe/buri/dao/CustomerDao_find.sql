f87d6d3bca5d7e1eca1be1efbb92a508b40b667f
Junichi Kato <j5ik2o@gmail.com>
2009-03-28 15:33:17 +0000

    refactor
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/branches/kato@520 1c266114-290a-0410-b1ce-a63319e86887

--- buri-s2dao-test/src/main/resources/example/org/escafe/buri/dao/CustomerDao_find.sql
+++ buri-s2dao-test/src/main/resources/example/org/escafe/buri/dao/CustomerDao_find.sql
@@ -1,42 +1,39 @@
-select
-		Customer.customerID as customerID
-	,Customer.customerName as customerName
-from
-	Customer
-	,BuriPathData
+SELECT
+	CUSTOMER.CUSTOMER_ID AS CUSTOMER_ID,
+	CUSTOMER.CUSTOMER_NAME AS CUSTOMER_NAME
+FROM
+	CUSTOMER,
+	BURI_PATH_DATA
 /*BEGIN*/
-where
-	
-	/*IF dto.customerID != null*/ customerID = /*dto.customerID*/0/*END*/
-	/*IF dto.customerID_not != null*/AND customerID != /*dto.customerID_not*/0/*END*/
-	/*IF dto.customerID_large != null*/AND  /*dto.customerID_large*/0 < customerID/*END*/
-	/*IF dto.customerID_moreLarge != null*/AND  /*dto.customerID_moreLarge*/0 <= customerID/*END*/
-	/*IF dto.customerID_from != null*/AND  /*dto.customerID_from*/0 <= customerID/*END*/
-	/*IF dto.customerID_to != null*/AND customerID <= /*dto.customerID_to*/0/*END*/
-	/*IF dto.customerID_moreSmall != null*/AND customerID <= /*dto.customerID_moreSmall*/0/*END*/
-	/*IF dto.customerID_small != null*/AND customerID < /*dto.customerID_small*/0/*END*/
-	/*IF dto.customerID_isNull != null*/AND customerID is null /*END*/
-	/*IF dto.customerID_isNotNull != null*/AND customerID is not null/*END*/
-	/*IF dto.customerID_in != null*/AND customerID in /*dto.customerID_in*/(0)/*END*/
+WHERE
+	/*IF dto.customerId != null*/ CUSTOMER_ID = /*dto.customerId*/0/*END*/
+	/*IF dto.customerId_not != null*/AND CUSTOMER_ID != /*dto.customerId_not*/0/*END*/
+	/*IF dto.customerId_large != null*/AND  /*dto.customerId_large*/0 < CUSTOMER_ID/*END*/
+	/*IF dto.customerId_moreLarge != null*/AND  /*dto.customerId_moreLarge*/0 <= CUSTOMER_ID/*END*/
+	/*IF dto.customerId_from != null*/AND  /*dto.customerId_from*/0 <= customerId/*END*/
+	/*IF dto.customerId_to != null*/AND CUSTOMER_ID <= /*dto.customerId_to*/0/*END*/
+	/*IF dto.customerId_moreSmall != null*/AND CUSTOMER_ID <= /*dto.customerId_moreSmall*/0/*END*/
+	/*IF dto.customerId_small != null*/AND CUSTOMER_ID < /*dto.customerId_small*/0/*END*/
+	/*IF dto.customerId_isNull != null*/AND CUSTOMER_ID IS NULL/*END*/
+	/*IF dto.customerId_isNotNull != null*/AND CUSTOMER_ID IS NOT NULL/*END*/
+	/*IF dto.customerId_in != null*/AND CUSTOMER_ID IN/*dto.customerId_in*/(0)/*END*/
 
-	
-	/*IF dto.customerName_matchFull != null*/AND customerName Like /*dto.customerName_matchFull*/'%TestData%'/*END*/
-	/*IF dto.customerName_matchFront != null*/AND customerName Like /*dto.customerName_matchFront*/'TestData%'/*END*/
-	/*IF dto.customerName_matchBack != null*/AND customerName Like /*dto.customerName_matchBack*/'%TestData'/*END*/
-	/*IF dto.customerName != null*/AND customerName = /*dto.customerName*/'TestData'/*END*/
-	/*IF dto.customerName_not != null*/AND customerName != /*dto.customerName_not*/'TestData'/*END*/
-	/*IF dto.customerName_large != null*/AND  /*dto.customerName_large*/'TestData' < customerName/*END*/
-	/*IF dto.customerName_moreLarge != null*/AND  /*dto.customerName_moreLarge*/'TestData' <= customerName/*END*/
-	/*IF dto.customerName_from != null*/AND  /*dto.customerName_from*/'TestData' <= customerName/*END*/
-	/*IF dto.customerName_to != null*/AND customerName <= /*dto.customerName_to*/'TestData'/*END*/
-	/*IF dto.customerName_moreSmall != null*/AND customerName <= /*dto.customerName_moreSmall*/'TestData'/*END*/
-	/*IF dto.customerName_small != null*/AND customerName < /*dto.customerName_small*/'TestData'/*END*/
-	/*IF dto.customerName_isNull != null*/AND customerName is null /*END*/
-	/*IF dto.customerName_isNotNull != null*/AND customerName is not null/*END*/
-	/*IF dto.customerName_in != null*/AND customerName in /*dto.customerName_in*/('TestData')/*END*/
+	/*IF dto.customerName_matchFull != null*/AND CUSTOMER_NAME LIKE/*dto.customerName_matchFull*/'%TestData%'/*END*/
+	/*IF dto.customerName_matchFront != null*/AND CUSTOMER_NAME LIKE/*dto.customerName_matchFront*/'TestData%'/*END*/
+	/*IF dto.customerName_matchBack != null*/AND CUSTOMER_NAME LIKE/*dto.customerName_matchBack*/'%TestData'/*END*/
+	/*IF dto.customerName != null*/AND CUSTOMER_NAME = /*dto.customerName*/'TestData'/*END*/
+	/*IF dto.customerName_not != null*/AND CUSTOMER_NAME != /*dto.customerName_not*/'TestData'/*END*/
+	/*IF dto.customerName_large != null*/AND  /*dto.customerName_large*/'TestData' < CUSTOMER_NAME/*END*/
+	/*IF dto.customerName_moreLarge != null*/AND  /*dto.customerName_moreLarge*/'TestData' <= CUSTOMER_NAME/*END*/
+	/*IF dto.customerName_from != null*/AND  /*dto.customerName_from*/'TestData' <= CUSTOMER_NAME/*END*/
+	/*IF dto.customerName_to != null*/AND CUSTOMER_NAME <= /*dto.customerName_to*/'TestData'/*END*/
+	/*IF dto.customerName_moreSmall != null*/AND CUSTOMER_NAME <= /*dto.customerName_moreSmall*/'TestData'/*END*/
+	/*IF dto.customerName_small != null*/AND CUSTOMER_NAME < /*dto.customerName_small*/'TestData'/*END*/
+	/*IF dto.customerName_isNull != null*/AND CUSTOMER_NAME IS NULL/*END*/
+	/*IF dto.customerName_isNotNull != null*/AND CUSTOMER_NAME IS NOT NULL/*END*/
+	/*IF dto.customerName_in != null*/AND CUSTOMER_NAME IN/*dto.customerName_in*/('TestData')/*END*/
 
-	AND Customer.customerID = BuriPathData.pkeyNum
-	/*IF paths != null*/AND BuriPathData.PathName in /*paths*/('buri.path.names')/*END*/
-	
+	AND CUSTOMER.CUSTOMER_ID = BURI_PATH_DATA.PKEY_NUM
+	/*IF paths != null*/AND BURI_PATH_DATA.PATH_NAME IN/*paths*/('buri.path.names')/*END*/	
 /*END*/
 /*$dto.orderList*/
