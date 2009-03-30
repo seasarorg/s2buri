a1a1c4644409abff3985c2918620c2b3e965da97
Junichi Kato <j5ik2o@gmail.com>
2009-03-30 06:07:47 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/branches/kato@530 1c266114-290a-0410-b1ce-a63319e86887

--- buri-s2dao-test/src/main/java/example/org/escafe/buri/dto/ShippingSetDto.java
+++ buri-s2dao-test/src/main/java/example/org/escafe/buri/dto/ShippingSetDto.java
@@ -8,23 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShippingSetDto extends ShippingDto {
-    private List items = new ArrayList();
+	private List<ShippingItemDto> items = new ArrayList<ShippingItemDto>();
 
-    public List getItems() {
-        return items;
-    }
+	public List<ShippingItemDto> getItems() {
+		return items;
+	}
 
-    public void setItems(List items) {
-        this.items = items;
-    }
-    
-    @Override
-    public String toString() {
-        StringBuffer buff = new StringBuffer("{");
-        buff.append(super.toString());
-        buff.append("/items=").append(items);
-        buff.append("}");
-        return buff.toString();
-    }
-    
+	public void setItems(List<ShippingItemDto> items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer buff = new StringBuffer("{");
+		buff.append(super.toString());
+		buff.append("/items=").append(items);
+		buff.append("}");
+		return buff.toString();
+	}
 }
