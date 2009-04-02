0a55026aaaba3ba3592abd92436f7c7d75a3c5b2
Junichi Kato <j5ik2o@gmail.com>
2009-04-02 06:39:08 +0000

    Copyrightを追加しました。って、このコピーライトでいいかな。。。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2buri/trunk@584 1c266114-290a-0410-b1ce-a63319e86887

--- buri-core-test/src/main/java/example/org/escafe/buri/entity/OrderDetail.java
+++ buri-core-test/src/main/java/example/org/escafe/buri/entity/OrderDetail.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package example.org.escafe.buri.entity;
 
 import javax.persistence.Column;
@@ -9,8 +24,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-
 
 @Entity
 @Table(name = "ORDER_DETAIL")
@@ -29,7 +42,7 @@ public class OrderDetail {
 
 	@Column(precision = 19, nullable = true, unique = false)
 	public Long orderTitleId;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "ORDER_TITLE_ID", referencedColumnName = "ORDER_TITLE_ID")
 	public OrderTitle orderTitle;
